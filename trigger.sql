7d0b1af6cfde2f6915bef9160e7cb7ea5c038711
P-Trepovskiy <91117654+P-Trepovskiy@users.noreply.github.com>
2024-01-19 18:50:04 +0200

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -4,7 +4,7 @@ RETURNS TRIGGER AS $$
 BEGIN
     IF TG_OP = 'UPDATE' THEN
         INSERT INTO features (store, date, temperature, fuel_price, cpi)
-        VALUES (NEW.store_id, NEW.date, NEW.temperature, OLD.fuel_price, NEW.fuel_price, NEW.cpi);
+        VALUES (NEW.store, NEW.date, NEW.temperature, NEW.fuel_price, NEW.cpi);
     END IF;
     RETURN NEW;
 END;
@@ -13,4 +13,4 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER fuel_price_change_trigger
 AFTER UPDATE OF fuel_price ON features
 FOR EACH ROW
-EXECUTE FUNCTION log_fuel_price_changes();ges();
+EXECUTE FUNCTION log_fuel_price_changes();
